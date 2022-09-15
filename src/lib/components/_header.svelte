bc041d1e06166c6ef1e3d7738849f99f0d0fe087
Oliver KEB <oliver.keb@icloud.com>
2022-09-15 08:18:47 +0200

    :lipstick: style(footer): restyle the footer

--- src/lib/components/_header.svelte
+++ src/lib/components/_header.svelte
@@ -1,4 +1,4 @@
-<header class="px-12 dark:bg-black bg-white grid grid-cols-3 font-normal no-underline <lg:py-4 lg:py-6 dark:text-white">
+<header class="px-12 dark:bg-black bg-white grid grid-cols-3 font-normal border-1 border-gray-800 <lg:py-4 lg:py-6 dark:text-white">
   <div class="flex content-center">
     <a class="nav-link text-2xl font-bold" href="/">LAMED</a>
   </div>
@@ -10,6 +10,6 @@
   </div>
   <div class="flex content-center justify-end">
     <a href="/connexion" class="nav-link">Connexion</a>
-    <a href="#newsletter" class="nav-link inline-flex items-center justify-center px-4 py-2 ml-8 text-base font-medium text-white bg-yellow-800 border border-transparent rounded-md shadow-sm whitespace-nowrap hover:bg-indigo-700"> Newsletter </a>
+    <a href="#newsletter" class="nav-link inline-flex items-center justify-center px-4 py-2 ml-8 text-base font-medium text-white bg-light-800 border border-transparent rounded-md whitespace-nowrap hover:bg-blue-200"> Newsletter </a>
   </div>
 </header>
