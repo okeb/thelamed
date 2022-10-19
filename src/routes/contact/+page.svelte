bfddf7c3e09803d78c6ad1566b701cc51f5f72f4
Oliver KEB <oliver.keb@icloud.com>
2022-10-19 22:30:32 +0200

    🌈 style(rubrique): style index page

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,66 +1,66 @@
 <div class="py-12 dark:bg-black">
-    <h1 class=" sans-serif text-gray-600 font-bold text-5xl text-center">
+    <h1 class=" font-lato text-gray-600 font-semibold text-5xl text-center">
         Vous avez une préoccupation
     </h1>
     <div class=" my-24 mx-12 grid grid-cols-2">
         <div class="">
             <div class="grow-0 shrink-0 basis-auto mb-12 md:mb-0 w-full md:w-7/12 px-3 lg:px-6">
                 <h2 class="text-3xl font-bold mb-6">Les questions fréquentes</h2>
-                <p class="font-bold mb-2">Anim pariatur cliche reprehenderit?</p>
-                <p class="text-gray-500 mb-6">
+                <p class="font-bold mb-2 font-lato">Anim pariatur cliche reprehenderit?</p>
+                <p class="text-gray-500 mb-6 font-lato">
                 Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt
                 autem numquam dolore molestias aperiam culpa alias veritatis
                 architecto eos, molestiae vitae ex eligendi libero eveniet
                 dolorem, doloremque rem aliquid perferendis.
                 </p>
-                <p class="font-bold mb-2">Non cupidatat skateboard dolor brunch?</p>
-                <p class="text-gray-500 mb-6">
+                <p class="font-bold mb-2 font-lato">Non cupidatat skateboard dolor brunch?</p>
+                <p class="text-gray-500 mb-6 font-lato">
                 Distinctio corporis, iure facere ducimus quos consectetur ipsa ut
                 magnam autem doloremque ex! Id, sequi. Voluptatum magnam sed fugit
                 iusto minus et suscipit? Minima sunt at nulla tenetur, numquam
                 unde quod modi magnam ab deserunt ipsam sint aliquid dolores
                 libero repellendus cupiditate mollitia quidem dolorem odit
                 </p>
-                <p class="font-bold mb-2">
+                <p class="font-bold mb-2 font-lato">
                 Praesentium voluptatibus temporibus consequatur non aspernatur?
                 </p>
-                <p class="text-gray-500 mb-6">
+                <p class="text-gray-500 mb-6 font-lato">
                 Minima sunt at nulla tenetur, numquam unde quod modi magnam ab
                 deserunt ipsam sint aliquid dolores libero repellendus cupiditate
                 mollitia quidem dolorem.
                 </p>
 
-                <p class="font-bold mb-2">
+                <p class="font-bold mb-2 font-lato">
                 Voluptatum magnam sed fugit iusto minus et suscipit?
                 </p>
-                <p class="text-gray-500">
+                <p class="text-gray-500 font-lato">
                 Laudantium perferendis, est alias iure ut veniam suscipit dolorem
                 fugit. Et ipsam corporis earum ea ut quae cum non iusto blanditiis
                 ipsum dolor eius reiciendis, velit adipisci quas.
                 </p>
             </div>
         </div>
-        <div class="px-3 lg:px-6 pt-6 font-sans">
+        <div class="px-3 lg:px-6 pt-6 font-lato">
             <h2 class="font-bold mb-6 text-2xl">Ne trouvez-vous pas la réponse à votre question ? Écrivez-nous.</h2>
-            <form>
-            <div class="mb-4">
-                <input type="text" class="input caret-yellow-500" id="exampleInput7"
-                placeholder="Nom & Prenom">
-            </div>
-            <div class="mb-4">
-                <input type="email" class="input caret-yellow-500" id="exampleInput8"
-                placeholder="Adresse email">
-            </div>
-            <div class="mb-2">
-                <textarea class="input caret-yellow-500" id="exampleFormControlTextarea13" rows="3" placeholder="Votre message"></textarea>
-            </div>
-            <div class="text-center mb-6">
-                <input type="checkbox"
-                class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
-                id="exampleCheck87" checked>
-                <label class="form-check-label inline-block text-gray-800" for="exampleCheck87">Je confirme vouloir envoyer ce message</label>
-            </div>
-            <button type="submit" class="w-full whitespace-nowrap inline-flex items-center justify-center px-4 py-3 border border-transparent rounded-lg shadow-sm text-base font-medium text-gray-900 text-lg bg-indigo-600 hover:bg-indigo-700 dark:hover:bg-purple-800">Envoyez</button>
+            <form class="px-12">
+                <div class="mb-4">
+                    <input type="text" class="input caret-yellow-500" id="exampleInput7"
+                    placeholder="Nom & Prenom">
+                </div>
+                <div class="mb-4">
+                    <input type="email" class="input caret-yellow-500" id="exampleInput8"
+                    placeholder="Adresse email">
+                </div>
+                <div class="mb-2">
+                    <textarea class="input caret-yellow-500" id="exampleFormControlTextarea13" rows="3" placeholder="Votre message"></textarea>
+                </div>
+                <div class="text-center mb-6">
+                    <input type="checkbox"
+                    class="form-check-input appearance-none h-4 w-4 border border-gray-300 rounded-sm bg-white checked:bg-blue-600 checked:border-blue-600 focus:outline-none transition duration-200 mt-1 align-top bg-no-repeat bg-center bg-contain mr-2 cursor-pointer"
+                    id="exampleCheck87" checked>
+                    <label class="form-check-label inline-block text-gray-800" for="exampleCheck87">Je confirme vouloir envoyer ce message</label>
+                </div>
+                <button type="submit" class="w-full whitespace-nowrap inline-flex items-center justify-center px-4 py-3 border border-transparent rounded-lg shadow-sm text-base font-medium text-gray-900 text-lg bg-indigo-600 hover:bg-indigo-700 dark:hover:bg-purple-800">Envoyez</button>
             </form>
         </div>
     </div>
--- src/routes/enseignements/rubrique/[rubrique]/+page.svelte
+++ src/routes/enseignements/rubrique/[rubrique]/+page.svelte
@@ -1,25 +1,62 @@
 <script>
+    import Time from "svelte-time";
     export let data
 </script>
-<h1> Rubrique {data.rubrique}</h1>
-<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Doloribus asperiores vitae sed labore ipsum assumenda vero nesciunt facilis! Non laborum repellat aut quos itaque magnam ad quae repudiandae harum eos.</p>
-<div class="pl-20 my-14 nav-link">
+
+<div class="bg-red-200 grid py-20 md:grid-cols-3 grid-cols-1 lg:px-24 <lg:px-1">
+    <div class="col-span-2">
+        <h1 class="font-lato font-extrabold tracking-tight text-left lg:px-20 @md:px-12 px-7 pb-0">{data.rubrique}</h1>
+        <p class="font-lato text-left md:px-10 pb-10 text-xl">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repellat error quae voluptatibus porro, at, eum ut expedita dolorem laboriosam temporibus mollitia natus inventore nam deleniti illum, tempore omnis unde maxime.</p>
+    </div>
+    <div  class="col-span-1 <md:px-8">
+        <img src="/abstract-brainstorm.gif" alt="" srcset="">
+    </div>
+</div>
+<!-- <div class="pl-20 my-14 nav-link">
     <a href="/enseignements/">
         retour
     </a>
+</div> -->
+<div class="lg:px-48 @md:px-12 px-8">
+    <div class="col-span-4 mt-16 mb-16">
+        <h2 class="mt-0 ml-0 text-left font-sans text-3xl font-bold tracking-tight" style="margin-left:0 !important;">Les enseignements</h2>
+        <div class="flex justify-between -mt-2 py-0">
+            <p class="text-[25px] font-lato px-0 tracking-normal font-light" style="margin-left:0px !important;">Découvrez nos enseignements sur {data.rubrique }</p>
+        </div>
+    </div>
+    {#if data.posts.length}
+         <div class="grid md:grid-cols-2 grid-cols-1 gap-7">
+             {#each data.posts as post}
+              {#if post.meta.published == true}
+                <div class="col-span-1">
+                    <div class="p-0 grid md:grid-cols-3 grid-cols-1 gap-7 lg:mb-24 @md:mb-12 mb-6">
+                    <div class="h-[10rem] col-span-1 rounded-md bg-light-700 border-2 border-yellow-500 bg-origin-border bg-no-repeat bg-cover bg-center" style="background-image:url('{post.meta.image}')">
+                    </div>
+                    <div class="col-span-2">
+                        <h5 class="-ml-10 text-left font-sans text-xl font-bold tracking-tight leading-6" style="margin-left:0px !important">{post.meta.title}</h5>
+                        <!-- <p class="text-md font-lato px-0 text-gray-700 mb-3"  style="margin-left:0px !important">{ post.meta.summary }</p> -->
+                        <p class="text-md font-lato px-0 text-gray-700 mb-3"  style="margin-left:0px !important">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque quod nemo eius in at ipsum dolore!</p>
+                        <div class="">
+                        <p class="inline-block text-sm font-lato px-0 text-gray-400 inline-block" style="margin-left:0px !important;">
+                            <a href="/enseignements/rubrique/lol" class="text-sm font-lato px-0 text-gray-400 font-normal inline-block inline-block" style="text-decoration:none !important; margin-left:0px !important; opacity:.5">
+                                { post.meta.rubriques }
+                            </a>
+                            <span class="text-sm inline-block inline-block px-2" style="margin:0px !important; font-size:9px; opacity:.3">&nbsp;&bull;</span>
+                            <Time style="font-size:x-small; margin-left:0px !important; opacity:.2" timestamp="{ post.meta.date }" format="D&nbsp;MMM" />
+                        </p>
+                        <a href="{ post.meta.download }" target="_blank" rel="noopener noreferrer" class="underline-transparent  -mt-6">
+                            <div class="mt-6 font-lato text-left leading-3 font-bold flex items-center justify-start px-0 mx-0 mt-4 cursor-pointer">
+                            <svg width="28" height="28" viewBox="0 0 24 24" class="inline-block"><path fill="lightseagreen" d="M8 8a1 1 0 0 0 0 2h1a1 1 0 0 0 0-2Zm5 12H6a1 1 0 0 1-1-1V5a1 1 0 0 1 1-1h5v3a3 3 0 0 0 3 3h3v2a1 1 0 0 0 2 0V8.94a1.31 1.31 0 0 0-.06-.27v-.09a1.07 1.07 0 0 0-.19-.28l-6-6a1.07 1.07 0 0 0-.28-.19a.29.29 0 0 0-.1 0a1.1 1.1 0 0 0-.31-.11H6a3 3 0 0 0-3 3v14a3 3 0 0 0 3 3h7a1 1 0 0 0 0-2Zm0-14.59L15.59 8H14a1 1 0 0 1-1-1ZM14 12H8a1 1 0 0 0 0 2h6a1 1 0 0 0 0-2Zm6.71 6.29a1 1 0 0 0-1.42 0l-.29.3V16a1 1 0 0 0-2 0v2.59l-.29-.3a1 1 0 0 0-1.42 1.42l2 2a1 1 0 0 0 .33.21a.94.94 0 0 0 .76 0a1 1 0 0 0 .33-.21l2-2a1 1 0 0 0 0-1.42ZM12 18a1 1 0 0 0 0-2H8a1 1 0 0 0 0 2Z"/></svg>
+                            <span class="select-none ml-2" style="opacity:.5">Télécharger en PDF</span>
+                            </div>
+                        </a>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            {/if}
+         {/each}
+         </div>
+    {/if}
 </div>
-
-{#if data.posts.length}
-     {#each data.posts as post}
-          {#if post.meta.published == true}
-            <li  class="lg:px-12 <lg:px-8 list-none">
-                <h2>
-                <a href={post.path} class="text-2xl">
-                    {post.meta.title}
-                </a>
-                </h2>
-                Published {post.meta.date}
-            </li>
-        {/if}
-     {/each}
-{/if}
+<div class="h-20 w-full"></div>
