3e99692ea674374e567989b45fc55b928c04ba17
Oliver KEB <oliver.keb@icloud.com>
2022-11-28 09:16:09 +0100

    🌈 style(rubrique): design rubrique page

--- src/routes/enseignements/rubrique/+page.svelte
+++ src/routes/enseignements/rubrique/+page.svelte
@@ -1,4 +1,4 @@
-<div class="mt-20 px-28 pt-12">
+<div class="mt-20 px-28 pt-12 gap-y-16 grid-cols-1 grid pb-20">
     
     <h2 class="font-merri font-bold text-6xl pb-12">Découvrez toutes<br>nos <span class="underline-yellow-500 underline underline-offset-2 underline-2">rubriques</span></h2>
 
@@ -7,8 +7,11 @@
 
 
     <div class="grid grid-cols-1 gap-3 border border-solid border-dark-900 rounded-lg">
-        <div class="card-rubrique px-2 pb-6 pt-8  border border-transparent border-solid border-b-dark-900">
-            <h3 class="font-lato font-bold">Les fondamentaux</h3>
+        <div class="flex justify-between border border-transparent border-solid border-b-dark-900 pl-2 pr-12">
+            <div class="card-rubrique pb-6 pt-8">
+                <h3 class="font-lato font-bold">Les fondamentaux</h3>
+            </div>
+            <a href="/enseignements/rubrique/Les Fondamentaux" class="link_rubrique <md:hidden nav-link-light inline-flex items-center justify-center px-4 py-1.5 ml-4 text-base font-medium rounded-md whitespace-nowrap hover:bg-yellow-500 dark:border border-transparent dark:border-gray-600 dark:hover:border-transparent">Voir les enseignements</a>
         </div>
 
         <p class="font-lato py-6">Cette partie traite des sujets qui viennent affermir la foi des chrétiens. Ils sont nécessaires pour la croissance spirituelle et l’affermissement des croyant en Rabbi Yehowshuw‘a -Mashiyah. A l’exemple de la doctrine sur le Salut : <span class="italic font-merri text-sm font-light">C’est pourquoi, il nous faut prendre garde de plus près aux choses que nous avons entendues, de peur que nous les laissions s’échapper. Car, si la Parole prononcée par leur moyen des anges a été ferme, et si toute transgression et toute désobéissance ont reçu une juste rémunération, comment échapperons-nous si nous négligeons un si grand salut, qui a commencé à être annoncé d’abord par le Seigneur et qui nous a été confirmé par ceux qui l’avaient entendu ?</span> (Hébreux 2:1-3_BYM).</p>
@@ -16,57 +19,75 @@
     </div>
 
 
-    <div class="grid grid-cols-1 gap-3">
-        <div class="card-rubrique px-2 border-transparent rounded-md border-2 border-light-900 py-6">
-            <h3 class="font-lato font-bold">La doctrine Apostolique</h3>
+    <div class="grid grid-cols-1 gap-3 border border-solid border-dark-900 rounded-lg">
+        <div class="flex justify-between border border-transparent border-solid border-b-dark-900 pl-2 pr-12">
+            <div class="card-rubrique pb-6 pt-8">
+                <h3 class="font-lato font-bold">La Doctrine Apostolique</h3>
+            </div>
+            <a href="/enseignements/rubrique/La Doctrine Apostolique" class="link_rubrique <md:hidden nav-link-light inline-flex items-center justify-center px-4 py-1.5 ml-4 text-base font-medium rounded-md whitespace-nowrap hover:bg-yellow-500 dark:border border-transparent dark:border-gray-600 dark:hover:border-transparent">Voir les enseignements</a>
         </div>
-        <p class="font-lato">Ici, cette section aborde les questions d’ordre doctrinal qu’il est nécessaire de connaître pour défendre la Parole dans le cadre de l’assemblée du Seigneur : <span class="italic font-merri text-sm">Quiconque va au-delà et ne demeure pas dans la doctrine du Mashiah n’a pas Elohîm. Celui qui demeure dans la doctrine du Mashiah a le Père et le Fils. Si quelqu’un vient vers et n’apporte pas cette doctrine, ne le recevez pas dans votre maison et ne lui dites pas : Salut ! Car celui qui lui dit : Salut, participe à ses mauvaises œuvres.</span> (2 Yohanan [Jean] 1:9-11_BYM).</p>
-        <p>Codification : <span class="font-bold">Doc/XXX/YY</span></p>
+        <p class="font-lato  py-6">Ici, cette section aborde les questions d’ordre doctrinal qu’il est nécessaire de connaître pour défendre la Parole dans le cadre de l’assemblée du Seigneur : <span class="italic font-merri text-sm">Quiconque va au-delà et ne demeure pas dans la doctrine du Mashiah n’a pas Elohîm. Celui qui demeure dans la doctrine du Mashiah a le Père et le Fils. Si quelqu’un vient vers et n’apporte pas cette doctrine, ne le recevez pas dans votre maison et ne lui dites pas : Salut ! Car celui qui lui dit : Salut, participe à ses mauvaises œuvres.</span> (2 Yohanan [Jean] 1:9-11_BYM).</p>
+        <p class="pb-12 pt-2">Codification : <span class="font-bold">Doc/XXX/YY</span></p>
     </div>
 
 
-    <div class="grid grid-cols-1 gap-3">
-        <div class="card-rubrique rounded-md border-2 border-light-900 px-2 border-transparent py-6">
-            <h3 class="font-lato font-bold">L'Eschatologie</h3>
+    <div class="grid grid-cols-1 gap-3 border border-solid border-dark-900 rounded-lg">
+        <div class="flex justify-between border border-transparent border-solid border-b-dark-900 pl-2 pr-12">
+            <div class="card-rubrique pb-6 pt-8">
+                <h3 class="font-lato font-bold">L'Eschatologie</h3>
+            </div>
+            <a href="/enseignements/rubrique/L'Eschatologie" class="link_rubrique <md:hidden nav-link-light inline-flex items-center justify-center px-4 py-1.5 ml-4 text-base font-medium rounded-md whitespace-nowrap hover:bg-yellow-500 dark:border border-transparent dark:border-gray-600 dark:hover:border-transparent">Voir les enseignements</a>
         </div>
-        <p class="font-lato">Dans cette rubrique, tous les sujets enseignés seront en relation directe avec la Fin des temps : <span class="italic font-merri text-sm">Mais s’étant assis sur la Montagne des Oliviers, ses disciples vinrent à lui en particulier et lui dirent : Dis-nous quand ces choses arriveront-elles, et quel sera le signe de ton avènement et de l’achèvement de l’âge ?</span>  (Matthaios [Matthieu] 24:3_BYM).</p>
-        <p>Codification : <span class="font-bold">Esch/XXX/YY</span></p>
+        <p class="font-lato py-6">Dans cette rubrique, tous les sujets enseignés seront en relation directe avec la Fin des temps : <span class="italic font-merri text-sm">Mais s’étant assis sur la Montagne des Oliviers, ses disciples vinrent à lui en particulier et lui dirent : Dis-nous quand ces choses arriveront-elles, et quel sera le signe de ton avènement et de l’achèvement de l’âge ?</span>  (Matthaios [Matthieu] 24:3_BYM).</p>
+        <p class="pb-12 pt-2">Codification : <span class="font-bold">Esch/XXX/YY</span></p>
     </div>
 
-    <div class="grid grid-cols-1 gap-3">
-        <div class="card-rubrique rounded-md border-2 border-light-900 px-2 border-transparent py-6">
-            <h3 class="font-lato font-bold">L'Historique</h3>
+    <div class="grid grid-cols-1 gap-3 border border-solid border-dark-900 rounded-lg">
+        <div class="flex justify-between border border-transparent border-solid border-b-dark-900 pl-2 pr-12">
+            <div class="card-rubrique pb-6 pt-8">
+                <h3 class="font-lato font-bold">L'Historique</h3>
+            </div>
+            <a href="/enseignements/rubrique/L'Historique" class="link_rubrique <md:hidden nav-link-light inline-flex items-center justify-center px-4 py-1.5 ml-4 text-base font-medium rounded-md whitespace-nowrap hover:bg-yellow-500 dark:border border-transparent dark:border-gray-600 dark:hover:border-transparent">Voir les enseignements</a>
         </div>
-        <p class="font-lato">Dans cette rubrique, tous les sujets enseignés seront en relation directe avec la Fin des temps : <span class="italic font-merri text-sm">Car si ces choses sont en vous et y abondent, elles ne vous laisseront pas paresseux ni stériles pour la connaissance précise et correctes de notre Seigneur Yéhoshoua Mashiah</span> (2 Petros [2 Pierre] 1:8_BYM).</p>
-        <p>La codification des personnages : <span class="font-bold">Hist/Perso/XXX/YY</span></p>
-        <p>La codification des résumés des livres : <span class="font-bold">Hist/Livre/XXX/YY</span></p>
-        <p>La codification des villes : <span class="font-bold">Hist/ville/XXX/YY</span></p>
-    </div>
-
-
-    <div class="grid grid-cols-1 gap-3">
-        <div class="card-rubrique rounded-md border-2 border-light-900 px-2 border-transparent py-6">
-            <h3 class="font-lato font-bold">Les Réligions</h3>
-        </div>
-        <p class="font-lato">Dans cette section, nous aborderons les religions et leurs fausses doctrines : <span class="italic font-merri text-sm">Et participez pas aux œuvres stérile de la ténèbre, mais plutôt condamnez-les en effet !</span>   (Ephésiens 5:11_BYM).</p>
-        <p>La codification des religions : <span class="font-bold">Rel/XXX/YY</span></p>
+        <p class="font-lato py-6">Dans cette rubrique, tous les sujets enseignés seront en relation directe avec la Fin des temps : <span class="italic font-merri text-sm">Car si ces choses sont en vous et y abondent, elles ne vous laisseront pas paresseux ni stériles pour la connaissance précise et correctes de notre Seigneur Yéhoshoua Mashiah</span> (2 Petros [2 Pierre] 1:8_BYM).</p>
+        <p class="pt-2">La codification des personnages : <span class="font-bold">Hist/Perso/XXX/YY</span></p>
+        <p class="pt-2">La codification des résumés des livres : <span class="font-bold">Hist/Livre/XXX/YY</span></p>
+        <p class="pb-12 pt-2">La codification des villes : <span class="font-bold">Hist/ville/XXX/YY</span></p>
     </div>
 
 
-    <div class="grid grid-cols-1 gap-3">
-        <div class="card-rubrique rounded-md border-2 border-light-900 px-2 border-transparent py-6">
-            <h3 class="font-lato font-bold">Les Audios</h3>
+    <div class="grid grid-cols-1 gap-3 border border-solid border-dark-900 rounded-lg">
+        <div class="flex justify-between border border-transparent border-solid border-b-dark-900 pl-2 pr-12">
+            <div class="card-rubrique pb-6 pt-8">
+                <h3 class="font-lato font-bold">Les Réligions</h3>
+            </div>
+            <a href="/enseignements/rubrique/Les Réligions" class="link_rubrique <md:hidden nav-link-light inline-flex items-center justify-center px-4 py-1.5 ml-4 text-base font-medium rounded-md whitespace-nowrap hover:bg-yellow-500 dark:border border-transparent dark:border-gray-600 dark:hover:border-transparent">Voir les enseignements</a>
         </div>
-        <p class="font-lato">Cette rubrique permettra de publier des exhortations, des enseignements via des audios afin d’édifier le maximum des frères et les sœurs.</p>
-        <p>La codification des audios : <span class="font-bold">Aud/XXX/YY</span></p>
+        <p class="font-lato py-6">Dans cette section, nous aborderons les religions et leurs fausses doctrines : <span class="italic font-merri text-sm">Et participez pas aux œuvres stérile de la ténèbre, mais plutôt condamnez-les en effet !</span>   (Ephésiens 5:11_BYM).</p>
+        <p class="pb-12 pt-2">La codification des religions : <span class="font-bold">Rel/XXX/YY</span></p>
     </div>
 
 
-    <div class="grid grid-cols-1 gap-3">
-        <div class="card-rubrique rounded-md border-2 border-light-900 px-2 border-transparent py-6">
-            <h3 class="font-lato font-bold">Les Livres</h3>
+    <div class="grid grid-cols-1 gap-3 border border-solid border-dark-900 rounded-lg">
+        <div class="flex justify-between border border-transparent border-solid border-b-dark-900 pl-2 pr-12">
+            <div class="card-rubrique pb-6 pt-8">
+                <h3 class="font-lato font-bold">Les Audios</h3>
+            </div>
+            <a href="/enseignements/rubrique/Les Audios" class="link_rubrique <md:hidden nav-link-light inline-flex items-center justify-center px-4 py-1.5 ml-4 text-base font-medium rounded-md whitespace-nowrap hover:bg-yellow-500 dark:border border-transparent dark:border-gray-600 dark:hover:border-transparent">Voir les enseignements</a>
         </div>
-        <p class="font-lato">Dans cette dernière partie, nous mettrons des livres à la disposition des frères et des sœurs pour leurs édifications.</p>
-        <p>La codification des livres : <span class="font-bold">Liv/XXX/YY</span></p>
+        <p class="font-lato py-6">Cette rubrique permettra de publier des exhortations, des enseignements via des audios afin d’édifier le maximum des frères et les sœurs.</p>
+        <p class="pb-12 pt-2">La codification des audios : <span class="font-bold">Aud/XXX/YY</span></p>
+    </div>
+
+
+    <div class="grid grid-cols-1 gap-3 border border-solid border-dark-900 rounded-lg">
+        <div class="flex justify-between border border-transparent border-solid border-b-dark-900 pl-2 pr-12">
+            <div class="card-rubrique pb-6 pt-8">
+                <h3 class="font-lato font-bold">Les Livres</h3>
+            </div>
+            <a href="/enseignements/rubrique/Les Livres" class="link_rubrique <md:hidden nav-link-light inline-flex items-center justify-center px-4 py-1.5 ml-4 text-base font-medium rounded-md whitespace-nowrap hover:bg-yellow-500 dark:border border-transparent dark:border-gray-600 dark:hover:border-transparent">Voir les enseignements</a>
+        </div>
+        <p class="font-lato py-6">Dans cette dernière partie, nous mettrons des livres à la disposition des frères et des sœurs pour leurs édifications.</p>
+        <p class="pb-12 pt-2">La codification des livres : <span class="font-bold">Liv/XXX/YY</span></p>
     </div>
 </div>
